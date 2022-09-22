a723abda63fa71cd8df04101747d318d5dfa8fdc
Dmitry Polurezov <polurezov@rhythmic-rebellion.com>
2022-09-22 11:32:37 +0300

    project setup

---
+++ AnimalFacts/AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by Dmitry Polurezov on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
